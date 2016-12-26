55b1125fe1eed91228a1e5ec167ecd69e5a9789b
Qianxi Zhang <qianxi.zhang@hulu.com>
2016-12-26 13:06:48 +0800

    add jobfailure, taskAttempt, grep into mapreduce example

---
+++ hadoop/src/main/java/cn/chinahadoop/mapreduce/Grep.java
@@ -0,0 +1,87 @@
+package cn.chinahadoop.mapreduce;
+
+import java.util.Random;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.map.InverseMapper;
+import org.apache.hadoop.mapreduce.lib.map.RegexMapper;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.reduce.LongSumReducer;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.io.Text;
+
+public class Grep extends Configured implements Tool {
+	private Grep() {
+	} // singleton
+
+	public int run(String[] args) throws Exception {
+		if (args.length < 3) {
+			System.out.println("Grep <inDir> <outDir> <regex> [<group>]");
+			ToolRunner.printGenericCommandUsage(System.out);
+			return 2;
+		}
+
+		Path tempDir = new Path("grep-temp-" + Integer.toString(new Random().nextInt(Integer.MAX_VALUE)));
+
+		Configuration conf = getConf();
+		conf.set(RegexMapper.PATTERN, args[2]);
+		if (args.length == 4)
+			conf.set(RegexMapper.GROUP, args[3]);
+
+		Job grepJob = new Job(conf);
+
+		try {
+
+			grepJob.setJobName("grep-search");
+
+			FileInputFormat.setInputPaths(grepJob, args[0]);
+
+			grepJob.setMapperClass(RegexMapper.class);
+
+			grepJob.setCombinerClass(LongSumReducer.class);
+			grepJob.setReducerClass(LongSumReducer.class);
+
+			FileOutputFormat.setOutputPath(grepJob, tempDir);
+			grepJob.setOutputFormatClass(SequenceFileOutputFormat.class);
+			grepJob.setOutputKeyClass(Text.class);
+			grepJob.setOutputValueClass(LongWritable.class);
+
+			grepJob.waitForCompletion(true);
+
+			Job sortJob = new Job(conf);
+			sortJob.setJobName("grep-sort");
+
+			FileInputFormat.setInputPaths(sortJob, tempDir);
+			sortJob.setInputFormatClass(SequenceFileInputFormat.class);
+
+			sortJob.setMapperClass(InverseMapper.class);
+
+			sortJob.setNumReduceTasks(1); // write a single file
+			FileOutputFormat.setOutputPath(sortJob, new Path(args[1]));
+			sortJob.setSortComparatorClass( // sort by decreasing freq
+					LongWritable.DecreasingComparator.class);
+
+			FileSystem.get(conf).delete(new Path(args[1]),true);
+			
+			sortJob.waitForCompletion(true);
+		} finally {
+			FileSystem.get(conf).delete(tempDir, true);
+		}
+		return 0;
+	}
+
+	public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(new Configuration(), new Grep(), args);
+		System.exit(res);
+	}
+
+}
---
+++ hadoop/src/main/java/cn/chinahadoop/mapreduce/TaskAttemptTest.java
@@ -0,0 +1,84 @@
+package cn.chinahadoop.mapreduce;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class TaskAttemptTest extends Configured implements Tool {
+	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			int id = context.getTaskAttemptID().getId();
+			System.out.println("id:" + id);
+			if (id < 4)
+				System.exit(-1);
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
+		}
+	}
+
+	public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+
+	public int run(String[] args) throws Exception {
+		Configuration conf = getConf();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		if (otherArgs.length < 2) {
+			System.err.println("Usage: word count attempt test <in> [<in>...] <out>");
+			System.exit(2);
+		}
+		Job job = new Job(conf, "word count attempt test");
+		job.setJarByClass(TaskAttemptTest.class);
+		job.setMapperClass(TokenizerMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		for (int i = 0; i < otherArgs.length - 1; ++i) {
+			FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
+		}
+
+		Path outputPath = new Path(args[1]);
+
+		outputPath.getFileSystem(conf).delete(outputPath);
+
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[otherArgs.length - 1]));
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
+
+	public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(new Configuration(), new TaskAttemptTest(), args);
+		System.exit(res);
+	}
+}
